d31085ea790ee7456dc6d69cdd9618e983b2957b
Asalasfl <137333489+Asalasfl@users.noreply.github.com>
2023-07-05 12:50:01 -0500

    update config server for microservices

--- AccountsCustomerMicroservice/src/main/java/nttdata/com/client/feign/CreditCardClient.java
+++ AccountsCustomerMicroservice/src/main/java/nttdata/com/client/feign/CreditCardClient.java
@@ -1,21 +1,16 @@
 package nttdata.com.client.feign;
 
+import feign.Headers;
 import nttdata.com.dto.CreditCardDTO;
-import nttdata.com.model.CreditCard;
-import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
-import reactor.core.publisher.Flux;
+import reactivefeign.spring.config.ReactiveFeignClient;
 import reactor.core.publisher.Mono;
 
-//@Configuration
-@FeignClient(name = "micro-credit-card")
+@ReactiveFeignClient(name = "micro-credit-card")
 public interface CreditCardClient {
 
     @GetMapping("/api/credit-cards/credits-card")
     public Mono<CreditCardDTO> createCreditCard(@RequestBody CreditCardDTO creditCardDTO);
 
-    @GetMapping("/api/credit-cards/{id}")
-    Mono<CreditCardDTO> findByCreditCardId(@RequestBody String creditCardId);
 }
--- AccountsCustomerMicroservice/src/main/java/nttdata/com/client/feign/CreditClient.java
+++ AccountsCustomerMicroservice/src/main/java/nttdata/com/client/feign/CreditClient.java
@@ -1,17 +1,15 @@
 package nttdata.com.client.feign;
 
 import nttdata.com.dto.CreditDTO;
-import nttdata.com.model.Credit;
-import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import reactor.core.publisher.Flux;
+import reactivefeign.spring.config.ReactiveFeignClient;
 import reactor.core.publisher.Mono;
 
 //@Configuration
-@FeignClient(name = "micro-credit")
+@ReactiveFeignClient(name = "micro-credit")
 public interface CreditClient {
 
-    @GetMapping("/api/credits/{id}")
-   public Mono<Credit> createCredit(@RequestBody CreditDTO creditDTO);
+    @GetMapping("/api/credits/credits")
+   public Mono<CreditDTO> createCredit(@RequestBody CreditDTO creditDTO);
 }
